4a1f2d0dbe0934574d30a50d9e3fbd7774109dd1
Lucian Buzzo <lucian.buzzo@gmail.com>
2024-02-21 08:12:34 +0000

    feat: fix encrypted-regex check and add verbose mode (#2)
    * feat: fix encrypted-regex check and add verbose mode
    
    * fix: make main script file executable

--- ezsops.sh
+++ ezsops.sh
@@ -2,6 +2,25 @@
 
 set -e
 
+# Initialize verbose flag
+verbose=0
+
+# Parse command-line arguments
+for arg in "$@"; do
+  if [[ $arg == "-v" || $arg == "--verbose" ]]; then
+    echo "Verbose mode enabled"
+    verbose=1
+    break # Optional: remove if you want to check for other flags
+  fi
+done
+
+# Define verbose logging function
+log_verbose() {
+  if [[ $verbose -eq 1 ]]; then
+    echo "$@"
+  fi
+}
+
 ezsops_decrypt() {
   if [ -z "$1" ]; then
     echo "Usage: decrypt_sops <path/to/sops.filename.yml>"
@@ -32,14 +51,43 @@ ezsops_encrypt() {
   encrypted_file="${decrypted_file%.*}.yml"
   encrypted_file="sops.${decrypted_file##*/}"
 
+  log_verbose "Encrypting file: ${decrypted_file}"
+  log_verbose "Destination file: ${encrypted_file}"
+
   if [ ! -f "${decrypted_file}" ]; then
     echo "Error: Decrypted file '${decrypted_file}' not found."
     return 1
   fi
 
+  # Regex pattern to match
+  regex="^(data|stringData|secrets)$"
+
+  # Flag to indicate if a match is found
+  match_found=0
+
+  # Extract top-level keys using yq and check against regex
+  # This is done with process substitution to avoid creating a subshell, allowing us to correctly set `match_found`
+  while read key; do
+    if [[ $key =~ $regex ]]; then
+      log_verbose "Found a top-level key matching the encryption pattern: $key"
+      match_found=1
+      break
+    fi
+  done < <(yq e 'keys | .[]' "$decrypted_file")
+
+  # Check if a match was found
+  if [[ $match_found -eq 0 ]]; then
+    echo "Error: No top-level key found matching the encyption pattern: ${regex}"
+    echo "This could mean that your file will not be encrypted correctly. Aborting now."
+    exit 1
+  fi
+
   kms_file="$HOME/.sops_kms_arns"
 
+  log_verbose "Looking up KMS ARN file: ${kms_file}"
+
   if [ ! -f "${kms_file}" ]; then
+    log_verbose "KMS ARN file not found. Creating a new one."
     touch "${kms_file}"
   fi
 
@@ -80,10 +128,10 @@ ezsops_encrypt() {
     kms_arn="${new_kms_arn}"
   fi
 
-  sops -e --kms "${kms_arn}" --encrypted-regex '^(data|stringData|secrets)$' "${decrypted_file}" > "${encrypted_file}"
-  if ! grep -q "ENC\[" "${encrypted_file}"; then
-    echo "!!!WARNING!!!: The file '${encrypted_file}' does not seem to be encrypted correctly."
-  fi
+  log_verbose "Using KMS ARN: ${kms_arn}"
+
+  sops -e --kms "${kms_arn}" --encrypted-regex "${regex}" "${decrypted_file}" > "${encrypted_file}"
+
   echo "Encrypted '${decrypted_file}' to '${encrypted_file}' using KMS ARN '${kms_arn}'"
 }
 
